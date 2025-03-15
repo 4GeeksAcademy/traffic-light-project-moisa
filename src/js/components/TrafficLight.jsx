cc15a191d612a725fe821e7d61fd7500104315a7
Moyunlimited <moy.estime@gmail.com>
2025-03-15 17:49:07 +0000

    done

---
+++ src/js/components/TrafficLight.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+
+const TrafficLight = () => {
+    const [glowy, SetGlowy] = useState ("red") 
+    return (
+        <div className="trafficLight">
+            <div className="stick"></div>
+            <div className="lightgroup">
+                <div
+                    className={glowy == "red" ? "red light glowy" : "red light"} onClick={() => SetGlowy("red")}>
+                </div>
+                <div
+                    className={glowy == "yellow" ? "yellow light glowy" : "yellow light"} onClick={() => SetGlowy("yellow")}>
+                </div>
+                <div
+                    className={glowy == "green" ? "green light glowy" : "green light"} onClick={() => SetGlowy("green")}>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default TrafficLight;
